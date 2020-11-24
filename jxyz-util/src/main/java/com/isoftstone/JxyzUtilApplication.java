40c2137439bb4da7b1497ef9d18516f4ce6bf776
sn_baby <sn_baby@qq.com>
2020-11-24 16:36:48 +0800

    修改

---
+++ jxyz-util/src/main/java/com/isoftstone/JxyzUtilApplication.java
@@ -0,0 +1,13 @@
+package com.isoftstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxyzUtilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxyzUtilApplication.class, args);
+	}
+
+}
