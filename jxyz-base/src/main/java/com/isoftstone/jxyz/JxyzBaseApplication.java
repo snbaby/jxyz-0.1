8b22cab0bcb2d72e85497cb6ced9b9fd52591b30
sn_baby <sn_baby@qq.com>
2020-11-28 15:24:43 +0800

    修改

--- jxyz-base/src/main/java/com/isoftstone/jxyz/JxyzBaseApplication.java
+++ jxyz-base/src/main/java/com/isoftstone/jxyz/JxyzBaseApplication.java
@@ -3,28 +3,39 @@ package com.isoftstone.jxyz;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.github.drinkjava2.jdbpro.SqlOption;
 import com.github.drinkjava2.jsqlbox.DbContext;
 import com.github.drinkjava2.jtransactions.spring.SpringTxConnectionManager;
 
 @SpringBootApplication
 public class JxyzBaseApplication {
-    @Autowired
-    DataSource ds;
+	@Autowired
+	@Qualifier("masterDataSource")
+	DataSource masterDataSource;
 
-    public static void main(String[] args) {
-        SpringApplication.run(JxyzBaseApplication.class, args);
-    }
+	@Autowired
+	@Qualifier("slaveDataSource")
+	DataSource slaveDataSource;
 
-    @Bean
-    public DbContext createDefaultDbContext() {
-        DbContext ctx = new DbContext(ds);
-        ctx.setConnectionManager(SpringTxConnectionManager.instance());
-        ctx.setAllowShowSQL(false);
-        DbContext.setGlobalDbContext(ctx);// 设定静态全局上下文
-        return ctx;
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JxyzBaseApplication.class, args);
+	}
+
+	@Bean
+	public DbContext createDefaultDbContext() {
+		DbContext masterCtx = new DbContext(masterDataSource);
+		DbContext salveCtx = new DbContext(slaveDataSource);
+		DbContext[] slaveDbContext = { salveCtx };
+		masterCtx.setSlaves(slaveDbContext);
+		masterCtx.setMasterSlaveOption(SqlOption.USE_BOTH);
+		masterCtx.setConnectionManager(SpringTxConnectionManager.instance());
+		masterCtx.setAllowShowSQL(false);
+		DbContext.setGlobalDbContext(masterCtx);// 设定静态全局上下文
+		return masterCtx;
+	}
 }
---
+++ jxyz-base/src/main/java/com/isoftstone/jxyz/config/SlaveDataSourceConfig.java
@@ -0,0 +1,62 @@
+package com.isoftstone.jxyz.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class SlaveDataSourceConfig {
+	@Value("${jxyz.datasource.slave.driverClassName}")
+	private String driverClassName;
+
+	@Value("${jxyz.datasource.slave.url}")
+	private String url;
+
+	@Value("${jxyz.datasource.slave.username}")
+	private String username;
+
+	@Value("${jxyz.datasource.slave.password}")
+	private String password;
+
+	@Value("${jxyz.datasource.slave.hikari.connection-timeout}")
+	private Long connectionTimeout;
+	@Value("${jxyz.datasource.slave.hikari.auto-commit}")
+	private Boolean autoCommit;
+	@Value("${jxyz.datasource.slave.hikari.max-lifetime}")
+	private Long maxLifetime;
+	@Value("${jxyz.datasource.slave.hikari.pool-name}")
+	private String poolName;
+	@Value("${jxyz.datasource.slave.hikari.minimum-idle}")
+	private int minimumIdle;
+	@Value("${jxyz.datasource.slave.hikari.connection-test-query}")
+	private String connectionTestQuery;
+	@Value("${jxyz.datasource.slave.hikari.idle-timeout}")
+	private Long idleTimeout;
+	@Value("${jxyz.datasource.slave.hikari.maximum-pool-size}")
+	private int maximumPoolSize;
+
+	@Bean(name = "slaveDataSource")
+	public DataSource masterDataSource() {
+		HikariDataSource hikariDataSource = new HikariDataSource();
+		hikariDataSource.setDriverClassName(driverClassName);
+		hikariDataSource.setUsername(username);
+		hikariDataSource.setPassword(password);
+		hikariDataSource.setJdbcUrl(url);
+		
+		hikariDataSource.setConnectionTimeout(connectionTimeout);
+		hikariDataSource.setAutoCommit(autoCommit);
+		hikariDataSource.setMaxLifetime(maxLifetime);
+		hikariDataSource.setPoolName(poolName);
+		hikariDataSource.setMinimumIdle(minimumIdle);
+		
+		hikariDataSource.setConnectionTestQuery(connectionTestQuery);
+		hikariDataSource.setIdleTimeout(idleTimeout);
+		hikariDataSource.setMaximumPoolSize(maximumPoolSize);
+		return hikariDataSource;
+	}
+
+}
