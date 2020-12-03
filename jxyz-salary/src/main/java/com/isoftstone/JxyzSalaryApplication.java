e0f14242d681d1248de700683754017785c02388
sn_baby <sn_baby@qq.com>
2020-12-03 16:09:32 +0800

    修改

--- jxyz-salary/src/main/java/com/isoftstone/JxyzSalaryApplication.java
+++ jxyz-salary/src/main/java/com/isoftstone/JxyzSalaryApplication.java
@@ -1,13 +1,30 @@
 package com.isoftstone;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.github.drinkjava2.jsqlbox.DbContext;
+import com.github.drinkjava2.jtransactions.spring.SpringTxConnectionManager;
 
 @SpringBootApplication
 public class JxyzSalaryApplication {
+	@Autowired
+	DataSource ds;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JxyzSalaryApplication.class, args);
 	}
 
+	@Bean
+	public DbContext createDefaultDbContext() {
+		DbContext ctx = new DbContext(ds);
+		ctx.setConnectionManager(SpringTxConnectionManager.instance());
+		ctx.setAllowShowSQL(false);
+		DbContext.setGlobalDbContext(ctx);// 设定静态全局上下文
+		return ctx;
+	}
 }
---
+++ jxyz-salary/src/main/java/com/isoftstone/component/SalaryAnalysisComponent.java
@@ -0,0 +1,8 @@
+package com.isoftstone.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SalaryAnalysisComponent {
+	
+}
