4ebdaa3f3c855adec1e3fd5ec84e1b79b4c044c5
sn_baby <sn_baby@qq.com>
2020-12-03 15:40:25 +0800

    修改

---
+++ jxyz-salary/src/main/java/com/isoftstone/JxyzSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.isoftstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxyzSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxyzSalaryApplication.class, args);
+	}
+
+}
