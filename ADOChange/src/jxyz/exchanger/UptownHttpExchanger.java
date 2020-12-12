c5c27d8d1eed91b5d30e4fda6e806b8ca6d1dab0
liuyi3166 <liuyi7247@126.com>
2020-12-12 12:16:32 +0800

    添加同步表代码

---
+++ ADOChange/src/jxyz/exchanger/UptownHttpExchanger.java
@@ -0,0 +1,22 @@
+package jxyz.exchanger;
+
+import jxyz.utils.HttpUtil;
+import java.sql.Connection;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class UptownHttpExchanger implements Exchanger {
+    @Override
+    public void process(Connection connection) {
+    	//这里改变地址===================================================
+        String url = "http://127.0.0.1:18411/jxyz-etl/etl/download";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("qry", "select * from t_uptown_m;");
+        jsonObject.put("prefix", "TRUNCATE t_uptown_m;");
+        jsonObject.put("suffix", "");
+        jsonObject.put("table", "t_uptown_m");
+        String a = HttpUtil.doPost(url,jsonObject.toJSONString());
+        System.out.println("收到结果过返回===========》" + a);
+    }
+    
+}
